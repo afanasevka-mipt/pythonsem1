6cb7c9bf02d9ac84c3bce1293d624d29ec6d6673
afanasevka-mipt <a-k-a-2005@mail.ru>
2023-09-19 20:31:43 +0300

    initial commit

--- Sem2/ex10.py
+++ Sem2/ex10.py
@@ -1,6 +1,5 @@
 import turtle as tr
 tr.shape('turtle')
-n, k = 1, 1
 for i in range(3):
     for k in range(1, 361):
         tr.forward(1)
@@ -9,5 +8,3 @@ for i in range(3):
         tr.forward(1)
         tr.right(1)
     tr.left(60)
-tr.done(n)
-
