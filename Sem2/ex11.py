6ccb09da90b68b312e78f141c46dc56f62b58914
afanasevka-mipt <a-k-a-2005@mail.ru>
2023-09-19 20:48:22 +0300

    initial commit

---
+++ Sem2/ex11.py
@@ -0,0 +1,10 @@
+import turtle as tr
+tr.shape('turtle')
+tr.left(90)
+for i in range(10):
+    for k in range(1, 361):
+        tr.forward(0.3+i*0.1)
+        tr.left(1)
+    for k in range(1, 361):
+        tr.forward(0.3+i*0.1)
+        tr.right(1)
