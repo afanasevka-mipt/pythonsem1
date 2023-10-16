46b071928616282bc3b7d80a795e7f5210bb67f5
afanasevka-mipt <a-k-a-2005@mail.ru>
2023-10-16 17:55:13 +0300

    Класс сиквенс

---
+++ Sem5/Sequence.py
@@ -0,0 +1,49 @@
+class Sequence(object):
+    def __init__(self, string: str):
+        self.string = string
+
+    def transcribe(self) -> None:
+        raise NameError
+    
+    def hamming_distance(self, string2: str) -> int:
+        if len(self.string) != len(string2):
+            raise ValueError('line lengths are different')
+        else:
+            c = 0
+            for i in range(0, len(self.string)):
+                if self.string[i] != string2[i]:
+                    c += 1
+            self.h_d = c
+        return self.h_d
+
+class DNA(Sequence):
+    def count_nucleotides_DNA(self) -> dict:
+        c_a = self.string.count('A')
+        c_t = self.string.count('T')
+        c_g = self.string.count('G')
+        c_c = self.string.count('C')
+        self.count_DNA = {'A': c_a, 'T': c_t, 'G': c_g, 'C': c_c}
+        return self.count_DNA
+    
+    def transcribe_DNA(self) -> str:
+        self.string_tr_DNA = self.string.replace('T', 'U')
+        return self.string_tr_DNA
+    
+    def complement_dna(self) -> str:
+        self.string_cm = self.string
+        self.string_cm = self.string_cm.replace('A', '1')
+        self.string_cm = self.string_cm.replace('T', 'A')
+        self.string_cm = self.string_cm.replace('1', 'T')
+        self.string_cm = self.string_cm.replace('G', '2')
+        self.string_cm = self.string_cm.replace('C', 'G')
+        self.string_cm = self.string_cm.replace('2', 'C')
+        return self.string_cm
+
+class RNA(Sequence):
+    def transcribe_RNA(self) -> str:
+        self.string_tr_RNA = self.string.replace('U', 'T')
+        return self.string_tr_RNA
+
+s = DNA('ATGC')
+s.hamming_distance('AAGC')
+print(s.h_d)
